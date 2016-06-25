04bc5f4b608efa124d0e5ee4033555734d340816
Julien MONTIJANO <jmontija@e2r4p10.42.fr>
2016-06-25 08:03:39 +0200

    jfk

--- julio/cmd_info.py
+++ julio/cmd_info.py
@@ -10,6 +10,7 @@ def load_conf(file):
 		print("error config -> " + file)
 
 class cmd_info:
+
 	def __init__(self):
 		data = load_conf("config.yaml")
 		cmd = data.get("programs")
@@ -17,12 +18,6 @@ class cmd_info:
 		for k, v in cmd.iteritems():
 			cmd_class = cmd_data(k, v)
 			self.cmd[k] = cmd_class
-	def	start(self, line):
-		for k, v in self.cmd.iteritems():
-			cmd = self.cmd[k]
-			if (line and cmd.id == line):
-				cmd.start()
-				break
 
 	def	autostart(self):
 		for k, v in self.cmd.iteritems():
@@ -30,16 +25,18 @@ class cmd_info:
 			if (cmd.autostart):
 				cmd.start()
 
+	def	start(self, line):
+		for k, v in self.cmd.iteritems():
+			cmd = self.cmd[k]
+			if (line and cmd.id == line):
+				cmd.start()
+				break
+
 	def	restart(self, line):
 		for k, v in self.cmd.iteritems():
 			cmd = self.cmd[k]
 			if (line and cmd.id == line):
 				cmd.restart()
-
-	def	status(self):
-		for k, v in self.cmd.iteritems():
-			cmd = self.cmd[k]
-			cmd.show_status()
 
 	def	stop(self, line):
 		for k, v in self.cmd.iteritems():
@@ -47,5 +44,10 @@ class cmd_info:
 			if (line and cmd.id == line):
 				cmd.stop()
 				break
+
+	def	status(self):
+		for k, v in self.cmd.iteritems():
+			cmd = self.cmd[k]
+			cmd.show_status()
 
 
